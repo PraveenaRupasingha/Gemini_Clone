26ec5d049871b396dc9898d18207a3419a7ea558
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2024-05-24 19:55:51 +0530

    Main bottom component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -31,7 +31,19 @@ const Main = () => {
                     <p>Improve the readability of the following code</p>
                     <img src={assets.code_icon} alt=''/>
                 </div>
-                
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type='text'placeholder='Enter a prompt here'/>
+                        <div>
+                            <img src={assets.gallery_icon} alt=''/>
+                            <img src={assets.mic_icon} alt=''/>
+                            <img src={assets.send_icon} alt=''/>
+                        </div>
+                    </div>
+                    <p className='bottom-info'>
+                        Gemini may display inaccurate info, including about people, so double-check its responses
+                    </p>
+                </div>
             </div>
         </div>
     </div>
